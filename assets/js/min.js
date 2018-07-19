644be28113c8dc3b5cdac2d6534df711fc6599fa
israeldelmal <40775374+israeldelmal@users.noreply.github.com>
2018-07-19 11:23:59 -0600

    Mixen | Personal Academy | #1

---
+++ assets/js/min.js
@@ -0,0 +1,8 @@
+$('.item-menu').on('click', function(event) {
+	event.preventDefault();
+	let Item = $(this).attr('href');
+	
+	$('body, html').stop(true, true).animate({
+		scrollTop: $(Item).offset().top - 100
+	}, 1000);
+});
